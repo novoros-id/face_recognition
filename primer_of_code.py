7b26ea1aeb306f57207def4c36a6cd98808243af
novoros-id <novoros-id@inbox.ru>
2020-11-15 21:56:58 +0300

    add bb

--- primer_of_code.py
+++ primer_of_code.py
@@ -6,9 +6,12 @@ with open("embeddings/embeddings.pkl", "rb") as f:
     (saved_embeds, names) = pickle.load(f)
 
     print(saved_embeds.ndim)
-
+    print(saved_embeds.ndim + 1)
     print(len(saved_embeds))
     print(len(names))
+
+    for i in names:
+        print (i)
 
     print(type(saved_embeds))
     print(type(names))
@@ -16,17 +19,17 @@ with open("embeddings/embeddings.pkl", "rb") as f:
     # print(saved_embeds)
     # print(names)
 
-    shutil.copyfile("embeddings/embeddings.pkl", "embeddings/embeddings_prev.pkl")
-
-    print ("-----------------------")
-
-    new_embeds = np.vstack ((saved_embeds,saved_embeds))
-    new_names = names + names
-
-    print(new_embeds.ndim)
-
-    print(len(new_embeds))
-    print(len(new_names))
-
-    print(type(saved_embeds))
-    print(type(new_names))
+    # shutil.copyfile("embeddings/embeddings.pkl", "embeddings/embeddings_prev.pkl")
+    #
+    # print ("-----------------------")
+    #
+    # new_embeds = np.vstack ((saved_embeds,saved_embeds))
+    # new_names = names + names
+    #
+    # print(new_embeds.ndim)
+    #
+    # print(len(new_embeds))
+    # print(len(new_names))
+    #
+    # print(type(saved_embeds))
+    # print(type(new_names))
